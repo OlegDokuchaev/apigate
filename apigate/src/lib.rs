a06eb74d2def445d847c7c00f2eaf3c84955d3f2
dokuchaev <dokuchaev@akc.ru>
2026-04-08 13:09:13 +0300

    feat(apigate): update lib

--- apigate/src/lib.rs
+++ apigate/src/lib.rs
@@ -7,12 +7,12 @@ pub mod __private {
     pub use serde_urlencoded;
 }
 
+pub use apigate_core::balancing;
+pub use apigate_core::policy::Policy;
+pub use apigate_core::routing;
 pub use apigate_core::{
     ApigateError, App, AppBuilder, DstChunk, HookResult, MapResult, Method, PartsCtx, PipelineFn,
     PipelineFuture, PipelineResult, RequestScope, RewriteSpec, RewriteTemplate, RouteDef, Routes,
     SrcSeg, run,
 };
-pub use apigate_core::balancing;
-pub use apigate_core::policy::Policy;
-pub use apigate_core::routing;
 pub use apigate_macros::*;
