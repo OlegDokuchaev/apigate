9fd32887031f7fba80f4c0efd1e81f9dae8e907b
dokuchaev <dokuchaev@akc.ru>
2026-04-20 00:53:51 +0300

    feat(apigate): update imports

--- apigate/src/lib.rs
+++ apigate/src/lib.rs
@@ -12,9 +12,9 @@ pub use apigate_core::policy::Policy;
 pub use apigate_core::routing;
 pub use apigate_core::{
     ApigateBuildError, ApigateCoreError, ApigateError, ApigateFrameworkError, ApigatePipelineError,
-    App, AppBuilder, BaseUriParseError, DstChunk, HookResult, MapResult, Method, PartsCtx,
-    PipelineFn, PipelineFuture, PipelineResult, RequestScope, RewriteSpec, RewriteTemplate,
-    RouteDef, Routes, RuntimeEvent, RuntimeEventKind, RuntimeObserver, SrcSeg,
+    App, AppBuilder, Backend, BackendPool, BaseUriParseError, DstChunk, HookResult, MapResult,
+    Method, PartsCtx, PipelineFn, PipelineFuture, PipelineResult, RequestScope, RewriteSpec,
+    RewriteTemplate, RouteDef, Routes, RuntimeEvent, RuntimeEventKind, RuntimeObserver, SrcSeg,
     default_error_renderer, default_tracing_observer, run, run_router,
 };
 pub use apigate_macros::*;
